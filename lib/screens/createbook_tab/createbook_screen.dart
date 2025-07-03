74e6569e4dfc429ca9445ab262a2f1fc61b24f07
Han Jiyun <yunhit@sookmyung.ac.kr>
2025-07-03 11:18:11 +0900

    dd

--- lib/screens/createbook_tab/createbook_screen.dart
+++ lib/screens/createbook_tab/createbook_screen.dart
@@ -1,6 +1,6 @@
 import 'character_selection_screen.dart';
 import 'gender_age_screen.dart';
-import 'lesson_screen.dart';
+import 'lesson_selection_screen.dart';
 import 'voice_selection_screen.dart';
 import '../../widgets/constants.dart';
 import 'package:flutter/material.dart';
@@ -25,7 +25,6 @@ class _CreateBookScreenState extends State<CreateBookScreen> {
   bool isStep3Completed = false;
   double progress = 0.15; //프로그레스 상태 변수 추가
 
-  // STEP 1: 성별/연령 선택 핸들러
   void _onGenderSelected(String gender) {
     setState(() {
       selectedGender = gender;
@@ -47,7 +46,6 @@ class _CreateBookScreenState extends State<CreateBookScreen> {
     }
   }
 
-  // STEP 2: 교훈 선택 핸들러
   void _onLessonSelected(String lesson) {
     setState(() {
       selectedLesson = lesson;
@@ -56,16 +54,14 @@ class _CreateBookScreenState extends State<CreateBookScreen> {
     });
   }
 
-  // STEP 3: 주인공 선택 핸들러
   void _onCharacterSelected(String character) {
     setState(() {
       selectedCharacter = character;
       isStep3Completed = true;
-      progress = 0.9; // 프로그레스 상태 변경
+      progress = 0.9;
     });
   }
 
-  // STEP 4: 목소리 선택 핸들러
   void _onVoiceSelected(String? voice) {
     setState(() {
       selectedVoice = voice ?? '';
@@ -73,7 +69,6 @@ class _CreateBookScreenState extends State<CreateBookScreen> {
     });
   }
 
-  // 뒤로 가기 콜백들
   void _onBackToGenderAge() {
     setState(() {
       isStep1Completed = false;
@@ -103,91 +98,122 @@ class _CreateBookScreenState extends State<CreateBookScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: AppColors.backgroundColor,
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Column(
-              children: [
-                //프로그레스 바 표시
-                DinosaurProgressBar(progress: progress),
-                SizedBox(height: 30),
-                // STEP 1: 성별/연령대 선택
-                if (!isStep1Completed)
-                  Column(
-                    children: [
-                      buildSectionTitle('독자의 성별과 연령대를 알려주세요!'),
-                      SizedBox(height: 30),
-                      GenderAgeSelectionScreen(
-                        onGenderSelected: _onGenderSelected,
-                        onAgeGroupSelected: _onAgeGroupSelected,
-                        selectedGender: selectedGender,
-                        selectedAgeGroup: selectedAgeGroup,
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 50),
+                    DinosaurProgressBar(progress: progress),
+                    const SizedBox(height: 10),
+                    if (!isStep1Completed)
+                      Column(
+                        children: [
+                          buildSectionTitle('독자의 성별과 연령대를 알려주세요!'),
+                          const SizedBox(height: 30),
+                          GenderAgeSelectionScreen(
+                            onGenderSelected: _onGenderSelected,
+                            onAgeGroupSelected: _onAgeGroupSelected,
+                            selectedGender: selectedGender,
+                            selectedAgeGroup: selectedAgeGroup,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                // STEP 2: 교훈 선택
-                if (isStep1Completed && !isStep2Completed)
-                  Column(
-                    children: [
-                      buildSectionTitle('생성할 동화의 교훈을 선택해 주세요!'),
-                      LessonSelectionScreen(
-                        onLessonSelected: _onLessonSelected,
-                        selectedLesson: selectedLesson,
-                        selectedGender: selectedGender,
-                        selectedAgeGroup: selectedAgeGroup,
-                        onBackToLesson: _onBackToLesson,
-                        onBackToGenderAge: _onBackToGenderAge,
+                    if (isStep1Completed && !isStep2Completed)
+                      Column(
+                        children: [
+                          buildSectionTitle('생성할 동화의 교훈을 선택해 주세요!'),
+                          LessonSelectionScreen(
+                            onLessonSelected: _onLessonSelected,
+                            selectedLesson: selectedLesson,
+                            selectedGender: selectedGender,
+                            selectedAgeGroup: selectedAgeGroup,
+                            onBackToLesson: _onBackToLesson,
+                            onBackToGenderAge: _onBackToGenderAge,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                // STEP 3: 동물 선택
-                if (isStep1Completed && isStep2Completed && !isStep3Completed)
-                  Column(
-                    children: [
-                      buildSectionTitle('생성할 동화의 주인공을 선택해 주세요!'),
-                      CharacterSelectionScreen(
-                        onCharacterSelected: _onCharacterSelected,
-                        selectedCharacter: selectedCharacter,
-                        selectedLesson: selectedLesson,
-                        selectedGender: selectedGender,
-                        selectedAgeGroup: selectedAgeGroup,
-                        onBackToLesson: _onBackToLesson,
-                        onBackToGenderAge: _onBackToGenderAge,
+                    if (isStep1Completed &&
+                        isStep2Completed &&
+                        !isStep3Completed)
+                      Column(
+                        children: [
+                          buildSectionTitle('생성할 동화의 주인공을 선택해 주세요!'),
+                          CharacterSelectionScreen(
+                            onCharacterSelected: _onCharacterSelected,
+                            selectedCharacter: selectedCharacter,
+                            selectedLesson: selectedLesson,
+                            selectedGender: selectedGender,
+                            selectedAgeGroup: selectedAgeGroup,
+                            onBackToLesson: _onBackToLesson,
+                            onBackToGenderAge: _onBackToGenderAge,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                // STEP 4: 목소리 선택
-                if (isStep1Completed && isStep2Completed && isStep3Completed)
-                  Column(
-                    children: [
-                      buildSectionTitle('동화를 읽어줄 목소리를 선택하세요!'),
-                      VoiceSelectionScreen(
-                        selectedVoice: selectedVoice,
-                        onVoiceSelected: _onVoiceSelected,
-                        onBackToCharacterSelection: _onBackToCharacterSelection,
-                        onBackToLesson: _onBackToLesson,
-                        onBackToGenderAge: _onBackToGenderAge,
-                        selectedCharacter: selectedCharacter,
-                        selectedLesson: selectedLesson,
-                        selectedGender: selectedGender,
-                        selectedAgeGroup: selectedAgeGroup,
+                    if (isStep1Completed &&
+                        isStep2Completed &&
+                        isStep3Completed)
+                      Column(
+                        children: [
+                          buildSectionTitle('동화를 읽어줄 목소리를 선택하세요!'),
+                          VoiceSelectionScreen(
+                            selectedVoice: selectedVoice,
+                            onVoiceSelected: _onVoiceSelected,
+                            onBackToCharacterSelection:
+                                _onBackToCharacterSelection,
+                            onBackToLesson: _onBackToLesson,
+                            onBackToGenderAge: _onBackToGenderAge,
+                            selectedCharacter: selectedCharacter,
+                            selectedLesson: selectedLesson,
+                            selectedGender: selectedGender,
+                            selectedAgeGroup: selectedAgeGroup,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-              ],
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
+          // 되돌아가기 버튼
+          Positioned(
+            left: 20,
+            top: 20,
+            child: SafeArea(
+              child: Container(
+                width: 40,
+                height: 40,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(0, 2),
+                      color: Colors.black.withOpacity(0.3),
+                    )
+                  ],
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_rounded, size: 20),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/home');
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  // title 위젯
   Widget buildSectionTitle(String title) {
     return Text(
       title,
-      style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold),
+      style: const TextStyle(fontSize: 33.5, fontWeight: FontWeight.bold),
     );
   }
 }
