997e30a80f2adcad0503d47da996a057bbe277fb
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-08-15 20:20:36 +0900

    feat: add ApiUrls class with base URL and endpoint definitions

--- lib/core/const/api_urls.dart
+++ lib/core/const/api_urls.dart
@@ -2,4 +2,5 @@ class ApiUrls {
   static final String baseUrl = 'http://localhost:8000';
   static final String loginUrl = '$baseUrl/login';
   static final String userInfoUrl = '$baseUrl/me';
+  static final String fetchMyVoicesUrl = '$baseUrl/user/myvoice';
 }
