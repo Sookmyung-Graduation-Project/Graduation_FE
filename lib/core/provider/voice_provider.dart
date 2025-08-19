82d96006876d5d7520d890d8b46b0f7420d55eb3
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-08-19 19:56:12 +0900

    feat: 음성 상태 관리를 위해 VoicesNotifier를 사용하도록 voice provider를 리팩터링

--- lib/core/provider/voice_provider.dart
+++ lib/core/provider/voice_provider.dart
@@ -1,6 +1,20 @@
-// voice_provider.dart
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:phonics/core/user/data/user_voice.dart';
+import 'package:phonics/core/utils/api_service.dart';
 
-final voicesProvider = StateProvider<List<VoiceItem>>((ref) => []);
+class VoicesNotifier extends StateNotifier<List<VoiceItem>> {
+  VoicesNotifier() : super([]);
+
+  Future<void> fetchVoices(String jwt) async {
+    final items = await ApiService.fetchMyVoices(jwt: jwt);
+    if (items != null) {
+      state = items.map((e) => VoiceItem.fromJson(e)).toList();
+    }
+  }
+}
+
+final voicesProvider = StateNotifierProvider<VoicesNotifier, List<VoiceItem>>(
+  (ref) => VoicesNotifier(),
+);
+
 final defaultVoiceIdProvider = StateProvider<String?>((ref) => null);
