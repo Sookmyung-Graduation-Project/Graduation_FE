2bdf20980c2d2d0f6a5e8247303a60a967ec1987
woojin-choi <dnwls0723@sookmyung.ac.kr>
2025-08-19 19:56:29 +0900

    feat: uploadIvcUrl endpoint 추가

--- lib/core/const/api_urls.dart
+++ lib/core/const/api_urls.dart
@@ -1,6 +1,7 @@
 class ApiUrls {
-  static final String baseUrl = 'http://localhost:8000';
+  static final String baseUrl = 'http://172.30.1.40:8000';
   static final String loginUrl = '$baseUrl/login';
   static final String userInfoUrl = '$baseUrl/me';
   static final String fetchMyVoicesUrl = '$baseUrl/user/myvoice';
+  static final String uploadIvcUrl = '$baseUrl/voice/ivc';
 }
