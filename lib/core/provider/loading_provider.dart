000dce3201f2afb22da0c2f6ff403a5f4b161c8d
Han Jiyun <yunhit@sookmyung.ac.kr>
2025-09-21 15:57:11 +0900

    로딩 인디케이터 적용
    loading_provider import 후
    await withLoading(ref, () async {
    });
    버튼 onPresed, 파일 로드 등 로딩 시간 걸리는 곳에 추가 시 적용됨 (home.dart, login_screen.dart 참고)

---
+++ lib/core/provider/loading_provider.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final isLoadingProvider = StateProvider<bool>((_) => false);
+
+Future<T> withLoading<T>(WidgetRef ref, Future<T> Function() task) async {
+  ref.read(isLoadingProvider.notifier).state = true;
+  try {
+    return await task();
+  } finally {
+    ref.read(isLoadingProvider.notifier).state = false;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:url_strategy/url_strategy.dart';
 import 'package:phonics/core/provider/jwt_provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:phonics/core/provider/loading_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -40,6 +41,36 @@ class MyApp extends ConsumerWidget {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
+      builder: (context, child) {
+        // 로딩 위젯 추가
+        final isLoading = ref.watch(isLoadingProvider);
+        debugPrint('isLoading: $isLoading');
+        return Stack(
+          children: [
+            child ?? const SizedBox.shrink(),
+            if (isLoading)
+              Positioned.fill(
+                child: Stack(
+                  children: [
+                    const ModalBarrier(
+                        color: Colors.black45, dismissible: false),
+                    Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: Image.asset(
+                          'assets/logo_icons/logo_loading.gif',
+                          width: 50,
+                          gaplessPlayback: true,
+                          filterQuality: FilterQuality.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        );
+      },
     );
   }
 }
